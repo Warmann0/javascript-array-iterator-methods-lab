3439e1ead42d0877cca4da83c817da27c429f4ce
warmann0 <mnw@warmann.org>
2025-01-30 12:24:00 -0700

    exersise 5 complet3

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ from an array of inventor objects
 - Utilize the Array.prototype.find() method to locate the object for the 
   inventor with the first name 'Ada'.
 - Assign the found inventor object to the variable inventorNamedAda
-*/
+
 
 let inventorNamedAda = inventors.find(inventor => inventor.first === 'Ada')
 
@@ -218,6 +218,69 @@ console.log('Exercise 4 correct result: ', {
 
 
 /*-------------------------------- Exersise 5 --------------------------------*/
+/*
+Exercise 5: Array.prototype.map()
+
+Use the Array.prototype.map() method to reformat each name in the 'people' 
+array. The goal is to convert names from "Last, First" format to "First Last" 
+format.
+
+Hint: Use the String.prototype.split() method to separate the first and last 
+      names. You can split the string using ', ' as the separator.
+      After splitting the names, rearrange them to the "First Last" format.
+*/
+
+let firstLast = people.map(person => {
+  let [last, first] = person.split(', ');
+  return `${first} ${last}`;
+});
+
+
+
+// Check your work:
+console.log('Exercise 5 my result: ', firstLast);
+console.log('Exercise 5 correct result: ', [
+  'Carl Becker',
+  'Samuel Beckett',
+  'Mick Beddoes',
+  'Henry Beecher',
+  'Ludwig Beethoven',
+  'Menachem Begin',
+  'Hilaire Belloc',
+  'Saul Bellow',
+  'Robert Benchley',
+  'Peter Benenson',
+  'David Ben-Gurion',
+  'Walter Benjamin',
+  'Tony Benn',
+  'Chester Bennington',
+  'Leana Benson',
+  'Silas Bent',
+  'Lloyd Bentsen',
+  'Ric Berger',
+  'Ingmar Bergman',
+  'Luciano Berio',
+  'Milton Berle',
+  'Irving Berlin',
+  'Eric Berne',
+  'Sandra Bernhard',
+  'Yogi Berra',
+  'Halle Berry',
+  'Wendell Berry',
+  'Erin Bethea',
+  'Aneurin Bevan',
+  'Ken Bevel',
+  'Joseph Biden',
+  'Ambrose Bierce',
+  'Steve Biko',
+  'Josh Billings',
+  'Frank Biondo',
+  'Augustine Birrell',
+  'Elk Black',
+  'Robert Blair',
+  'Tony Blair',
+  'William Blake',
+]);
 
 
 /*-------------------------------- Exersise 6 --------------------------------*/
